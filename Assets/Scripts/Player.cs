5a360ac5c21dec24b99fda309c4a0b5da71a389b
Roman Saykin <sc2aru@gmail.com>
2020-05-13 23:29:43 +0300

    created a player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private float _movementSpeed = 10f;
+    
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        
+        _rigidbody2D.velocity = new Vector2(horizontalInput * _movementSpeed, _rigidbody2D.velocity.y);
+    }
+}
