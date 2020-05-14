e7bcf6729adb1190d28747d9280dfe58c098e4c5
Roman Saykin <sc2aru@gmail.com>
2020-05-14 21:25:07 +0300

    added player movement script and input script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private float _movementSpeed = 5f;
+
+    private void OnEnable()
+    {
+        GetComponent<PlayerInput>().OnMovementButtonPressed += MoveCharacter;
+    }
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void MoveCharacter(float direction)
+    {
+        _rigidbody2D.velocity = new Vector2(direction * _movementSpeed, _rigidbody2D.velocity.y);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public event Action<float> OnMovementButtonPressed;
+    
+    private void Update()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        OnMovementButtonPressed?.Invoke(horizontalInput);
+    }
+}
