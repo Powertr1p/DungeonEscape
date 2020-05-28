e67853933b404da5ccd929a16c9d7e70b18f0cef
Powertr1p <sc2aru@gmail.com>
2020-05-28 11:44:31 +0300

    when monster reached a waypoint an idle animation now plays

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,14 @@ public class MossGiant : Enemy
     [SerializeField] private Transform _waypointB;
 
     private Transform _target;
+    private Animator _animator;
+
+    private const string Idle = "Idle";
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
 
     private void Start()
     {
@@ -17,11 +26,35 @@ public class MossGiant : Enemy
     
     protected override void Update()
     {
-        if (transform.position.x == _waypointA.transform.position.x)
-            _target = _waypointB.transform;
-        else if (transform.position.x == _waypointB.transform.position.x)
-            _target = _waypointA.transform;
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName(Idle)) return;
+        
+       
+        TryChangeMoveDirection();
+        Move();
+    }
 
+    private void TryChangeMoveDirection()
+    {
+        if (transform.position.x == _waypointA.position.x)
+        {
+            _target = _waypointB.transform;
+            ToggleIdleAnimation();
+        }
+        else if (transform.position.x == _waypointB.position.x)
+        {
+            _target = _waypointA.transform;
+            ToggleIdleAnimation();   
+        }
+    }
+
+    private void Move()
+    {
         transform.position = Vector2.MoveTowards(transform.position, _target.position, Speed * Time.deltaTime);
     }
+
+    private void ToggleIdleAnimation()
+    {
+        _animator.SetTrigger(Idle);
+    }
+    
 }
