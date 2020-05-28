625f1e8d4fd44ce09f43cb0c0ebb98a401807234
Powertr1p <sc2aru@gmail.com>
2020-05-28 10:05:27 +0300

    giant moss monster now moves between two waypoints

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
+    [SerializeField] private Transform _waypointA;
+    [SerializeField] private Transform _waypointB;
+
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = _waypointB.transform;
+    }
+    
+    
     protected override void Update()
     {
-        throw new System.NotImplementedException();
+        if (transform.position.x == _waypointA.transform.position.x)
+            _target = _waypointB.transform;
+        else if (transform.position.x == _waypointB.transform.position.x)
+            _target = _waypointA.transform;
+
+        transform.position = Vector2.MoveTowards(transform.position, _target.position, Speed * Time.deltaTime);
     }
 }
