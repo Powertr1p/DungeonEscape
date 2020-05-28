3ed4ca82ea7eff89cce7e7362d8586898649983d
Powertr1p <sc2aru@gmail.com>
2020-05-28 09:02:11 +0300

    added base enemy abstract class

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int Health;
+    [SerializeField] protected float Speed;
+    [SerializeField] protected int Gems;
+
+    protected virtual void Attack()
+    {
+        
+    }
+
+    protected abstract void Update();
+}
