d36dff3765cce35190b16500712d5bbb5abf26b2
Powertr1p <sc2aru@gmail.com>
2020-05-30 16:31:13 +0300

    refactored a enemy logic

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,20 +4,77 @@ using Cinemachine;
 using UnityEngine;
 
 namespace Enemy
-{ 
-    public abstract class Enemy : MonoBehaviour 
-    { 
+{
+    public abstract class Enemy : MonoBehaviour
+    {
         [SerializeField] protected int Health;
         [SerializeField] protected float Speed;
         [SerializeField] protected int Gems;
         [SerializeField] protected Transform WaypointA;
         [SerializeField] protected Transform WaypointB;
-        
-        protected virtual void Attack()
+
+        protected Transform Target;
+        protected Animator Animator;
+        protected SpriteRenderer Sprite;
+
+        private const string Idle = "Idle";
+
+        private void Start()
         {
-        
+            Init();
+
+            WaypointA.position = new Vector2(WaypointA.position.x, transform.position.y);
+            WaypointB.position = new Vector2(WaypointB.position.x, transform.position.y);
+
+            Target = WaypointB.transform;
         }
-        
-        protected abstract void Update();
-}
+
+        public virtual void Init()
+        {
+            Animator = GetComponentInChildren<Animator>();
+            Sprite = GetComponentInChildren<SpriteRenderer>();
+        }
+
+
+        protected virtual void Update()
+        {
+            if (Animator.GetCurrentAnimatorStateInfo(0).IsName(Idle)) return;
+
+            Flip();
+            TryChangeMoveDirection();
+            Move();
+        }
+
+        private void Flip()
+        {
+            if (Target.position == WaypointA.position)
+                Sprite.flipX = true;
+            else
+                Sprite.flipX = false;
+        }
+
+        private void TryChangeMoveDirection()
+        {
+            if (transform.position == WaypointA.position)
+            {
+                Target = WaypointB.transform;
+                ToggleIdleAnimation();
+            }
+            else if (transform.position == WaypointB.position)
+            {
+                Target = WaypointA.transform;
+                ToggleIdleAnimation();
+            }
+        }
+
+        private void Move()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        }
+
+        private void ToggleIdleAnimation()
+        {
+            Animator.SetTrigger(Idle);
+        }
+    }
 }
--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -5,68 +5,6 @@ namespace Enemy
 {
     public class Spider : Enemy
     {
-        private Transform _target;
-        private Animator _animator;
-        private SpriteRenderer _sprite;
-
-        private const string Idle = "Idle";
-
-        private void Awake()
-        {
-            _animator = GetComponentInChildren<Animator>();
-            _sprite = GetComponentInChildren<SpriteRenderer>();
-        }
-
-        private void Start()
-        {
-            WaypointA.position = new Vector2(WaypointA.position.x, transform.position.y);
-            WaypointB.position = new Vector2(WaypointB.position.x, transform.position.y);
-            
-            _target = WaypointB.transform;
-        }
-
-        protected override void Update()
-        {
-            if (_animator.GetCurrentAnimatorStateInfo(0).IsName(Idle)) return;
-        
-            Flip();
-            TryChangeMoveDirection();
-            Move();
-        }
-
-        private void TryChangeMoveDirection()
-        { 
-            if (transform.position == WaypointA.position) 
-            { 
-                _target = WaypointB.transform; 
-                ToggleIdleAnimation();
-            }
-            
-            else if (transform.position == WaypointB.position)
-            {
-                _target = WaypointA.transform;
-                ToggleIdleAnimation();
-            }
-        }
-
-        private void Move()
-        {
-            transform.position = Vector2.MoveTowards(transform.position, _target.position, Speed * Time.deltaTime);
-        }
-
-        private void ToggleIdleAnimation()
-        {
-            _animator.SetTrigger(Idle);
-        }
-
-        private void Flip()
-        { 
-            if (_target.position == WaypointA.position)
-                _sprite.flipX = true;
-            else
-                _sprite.flipX = false;
-        }
-            
     }
 }
 
