892c707f0c072178fa0418af1aa5f775c75f191a
Powertr1p <sc2aru@gmail.com>
2020-06-01 10:11:43 +0300

    added sword hitbox and changed sword layer

---
+++ Assets/Scripts/Player/SwordHitbox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+  public class SwordHitbox : MonoBehaviour
+  {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      Debug.Log("Hit: " + other.name);
+    }
+  }
+}
