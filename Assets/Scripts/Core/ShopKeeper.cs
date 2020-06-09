a187a22e07a316a249b39232b93cefdf050c7f5b
Powertr1p <sc2aru@gmail.com>
2020-06-09 15:27:04 +0300

    shopkeeper ui new appear when player enter area of shopkeeper

---
+++ Assets/Scripts/Core/ShopKeeper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class ShopKeeper : MonoBehaviour
+    {
+        [SerializeField] private GameObject _shopMenu;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            _shopMenu.SetActive(true);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            _shopMenu.SetActive(false);
+        }
+    }
+}
