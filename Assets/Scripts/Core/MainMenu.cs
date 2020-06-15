b2c767d67522969604dec968ae17e2e3e0247b62
Powertr1p <sc2aru@gmail.com>
2020-06-15 22:31:22 +0300

    added logic to start and quit game from main menu

---
+++ Assets/Scripts/Core/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
