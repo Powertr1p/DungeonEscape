11847f79a2ae0365073924102e937e819085688c
Powertr1p <sc2aru@gmail.com>
2020-07-06 13:03:44 +0300

    added canvas scalers to make UI more adaptive

---
+++ Assets/Scripts/Core/EndGame.cs
@@ -0,0 +1,15 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Core
+{
+    public class EndGame : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!GameEventsHandler.Instance.HasWinCondition) return;
+            
+            other.gameObject.GetComponent<IDamagable>().ApplyDamage(999);
+        }
+    }
+}
