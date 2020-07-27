281723fc4361563ecc50bf4045bd455dc87f895d
Powertr1p <sc2aru@gmail.com>
2020-07-27 19:28:08 +0300

    audio player now singleton (hate this shit) and plating music only in game  scene

---
+++ Assets/Scripts/Core/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer _instance;
+
+    private AudioSource _audio;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!_audio.isPlaying && SceneManager.GetActiveScene().buildIndex == 1)
+            _audio.Play();
+        
+        if (_audio.isPlaying && SceneManager.GetActiveScene().buildIndex != 1)
+            _audio.Stop();
+    }
+}
