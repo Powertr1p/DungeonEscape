84a38a0f985f63bfa8bfb4044d41d3a92144c0ee
Powertr1p <sc2aru@gmail.com>
2020-07-27 16:46:46 +0300

    fixed vertex distance of floor collider

--- Assets/Scripts/Hazards/Spikes.cs
+++ Assets/Scripts/Hazards/Spikes.cs
@@ -6,11 +6,13 @@ namespace Hazards
     [RequireComponent(typeof(Collider2D))]
     public class Spikes: MonoBehaviour
     {
+        private const int FatalDamage = 999;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.TryGetComponent(out IDamagable entity))
             {
-                entity.ApplyDamage(999);
+                entity.ApplyDamage(FatalDamage);
             }
         }
     }
