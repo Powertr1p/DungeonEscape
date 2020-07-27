1c9e8b3e7920179e91cc871590aefc9726c7e4f5
Powertr1p <sc2aru@gmail.com>
2020-07-27 16:18:46 +0300

    changed speed a bit

--- Assets/Scripts/Goals/Goals.cs
+++ Assets/Scripts/Goals/Goals.cs
@@ -4,7 +4,7 @@ public class Goals : MonoBehaviour
 {
    private Vector3 _endPosition;
    private Vector3 _startPosition = new Vector3(0, -90, 0);
-   private float _speed = 15f;
+   private float _speed = 30f;
    
    private bool _isMoving;
    private bool _isPanelOpened;
