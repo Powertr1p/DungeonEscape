4eed0bf8774af42174ca5a84a28254a76649294c
Powertr1p <sc2aru@gmail.com>
2020-08-03 09:57:46 +0300

    refactored a bit classes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,18 +7,14 @@ namespace Player
 {
     public class Player : MonoBehaviour, IDamagable
     {
-        
         public event Action<int> DamageTaken;
         public event Action Died;
-
         
         private int _livesRemaining = 4;
 
-
-
         public bool IsAlive => _livesRemaining > 0;
-        public int DiamondsCount { get; set; } = 600;
-
+        public int DiamondsCount { get; set; }
+        
         public void ApplyDamage(int damage)
         {
             if (!IsAlive) return;
